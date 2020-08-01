379a73101f5e991777477cb6d691ad4751f9cb39
trlis <trlis369@gmail.com>
2020-08-01 18:41:29 -0400

    updated happyB20.java

--- src/happyB20.java
+++ src/happyB20.java
@@ -1,5 +1,6 @@
 public class happyB20 {
     public static void main(String[] args) {
         System.out.println("Happy Batch 20");
+        System.out.println("Batch 20 has passed to the 2 part of the course");
     }
 }
